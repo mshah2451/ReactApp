3200c6ae6a4a7fa0354f3afaf01d91b93fea6deb
mukesh <mukesh@dpaulsonline.com>
2017-12-15 16:11:01 +0530

    add Config

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleClientID:"679553659427s.apps.googleusercontent.com",
+    googleSecretcode:"F",
+    mongooseURL:"mongodb://.com:29066/loginapp",
+    cookieKey:"asdfsddafsdafsd"
+}
